e9ec1c0ad9e7618c1263587c35c8614372dcfefa
Nandhu <85504447+NandhuImprefcet@users.noreply.github.com>
2021-06-28 11:50:10 +0530

    Create Insertion in BST.py

---
+++ Data Structure/Tree/Insertion in BST.py
@@ -0,0 +1,25 @@
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.left = None
+        self.right = None
+
+
+def insert(node, key):
+    if node is None:
+        return Node(key)
+    if key < node.key:
+        node.left = insert(node.left, key)
+    else:
+        node.right = insert(node.right, key)
+    return node
+
+
+
+root = Node(50)
+root = insert(root, 30)
+root = insert(root, 20)
+root = insert(root, 40)
+root = insert(root, 70)
+root = insert(root, 60)
+root = insert(root, 80)
